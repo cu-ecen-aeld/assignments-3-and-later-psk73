82f8ee1d271ba5f28f73d5af2f873899848a2063
Sreekanth Pasumarthy <pskanthu@gmail.com>
2023-11-18 09:38:09 -0800

    Updated finder-test.sh for assignment-4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,6 +5,12 @@
 set -e
 set -u
 
+rm -f /tmp/assignment4-result.txt
+myecho()
+{
+  echo $* |tee -a /tmp/assignment4-result.txt
+}
+
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
@@ -12,10 +18,10 @@ username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
+	myecho "Using default value ${WRITESTR} for string to write"
 	if [ $# -lt 1 ]
 	then
-		echo "Using default value ${NUMFILES} for number of files to write"
+		myecho "Using default value ${NUMFILES} for number of files to write"
 	else
 		NUMFILES=$1
 	fi	
@@ -27,7 +33,7 @@ fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
+myecho "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
@@ -43,18 +49,17 @@ then
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
 	if [ -d "$WRITEDIR" ]
 	then
-		echo "$WRITEDIR created"
+		myecho "$WRITEDIR created"
 	else
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
+#myecho "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
-
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR" 2>&1|tee -a /tmp/assignment4-result.txt
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
@@ -63,11 +68,11 @@ OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 rm -rf /tmp/aeld-data
 
 set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+myecho ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
+	myecho "success"
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	myecho "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
 	exit 1
 fi
