089c9ae9d453a9389025e322003b0fcece810b11
Sreekanth Pasumarthy <pskanthu@gmail.com>
2023-12-20 13:21:14 -0800

    assignment 7 part 1 updates

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -8,11 +8,29 @@
  *
  */
 
+//#define DEBUGPRINT
+
 #ifdef __KERNEL__
+
 #include <linux/string.h>
+#ifdef DEBUGPRINT
+#define PRINT printk
 #else
-#include <string.h>
+#define PRINT(msg,...)
 #endif
+
+#else
+
+#include <string.h>
+#include <stdio.h>
+#ifdef DEBUGPRINT
+#define PRINT printf
+#else
+#define PRINT(msg,...)
+#endif
+
+#endif
+
 
 #include "aesd-circular-buffer.h"
 
@@ -30,8 +48,52 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
     /**
-    * TODO: implement per description
+    * DONE: implement per description
     */
+    uint8_t index;
+    uint8_t endindex;
+    size_t curRunningOffset=0;
+    size_t prevRunningOffset=0;
+    struct aesd_buffer_entry *entryptr;
+
+    index = buffer->in_offs;
+
+    if(buffer->full)
+    {
+        //buffer full case
+        endindex = buffer->in_offs;
+    }
+    else if(buffer->in_offs == buffer->out_offs){
+        //buffer empty case
+        return NULL;
+    }
+    else {
+        endindex = (buffer->out_offs+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    PRINT("Search char_offset %d\n",(int)char_offset);
+    do {
+        PRINT("\t In Index: %d Out Index: %d Cur Index: %d End Index: %d\n",
+            buffer->in_offs, buffer->out_offs, index, endindex);
+        entryptr=&((buffer)->entry[index]);
+        prevRunningOffset = curRunningOffset;
+        curRunningOffset += entryptr->size;
+        PRINT("\t Char offset: %d Prev: %d  Cur %d\n",
+            (int)char_offset,(int)prevRunningOffset,(int)curRunningOffset);
+
+       if(char_offset+1 <= curRunningOffset)
+       {
+           //we found the entry
+           //calculate off set to return;
+           *entry_offset_byte_rtn = char_offset - prevRunningOffset;
+           PRINT("\t found returning offset %d\n",(int)(*entry_offset_byte_rtn));
+           return entryptr;
+       }
+       index = (index+1)%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+    while(index != endindex);
+
+    PRINT("\t Not found\n");
     return NULL;
 }
 
@@ -45,8 +107,37 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
     /**
-    * TODO: implement per description
+    * DONE: implement per description
     */
+
+    //copy new entry to circular buffer
+    buffer->entry[buffer->in_offs]= *add_entry;
+
+    PRINT("Before: in = %d out = %d full=%d\n", 
+        buffer->in_offs, buffer->out_offs, buffer->full);
+    PRINT("buf[%d] == %s\n",buffer->in_offs, buffer->entry[buffer->in_offs].buffptr);
+
+    //update offsets
+    buffer->in_offs = (buffer->in_offs +1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; 
+    if(buffer->full == true)
+    {
+        PRINT("Buffer overflow, lost previous entry \n");
+        buffer->out_offs = buffer->in_offs;
+    }
+    else
+    {
+        if(buffer->in_offs == buffer->out_offs)
+        {
+            PRINT("Buffer full\n ");
+            buffer->full = true;
+        }
+        else
+        {
+            buffer->full = false;
+        }
+    }
+    PRINT("After: in = %d out = %d full=%d\n\n", 
+        buffer->in_offs, buffer->out_offs, buffer->full);
 }
 
 /**
